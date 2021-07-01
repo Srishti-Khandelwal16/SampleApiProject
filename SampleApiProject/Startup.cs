7a96a1c4b0d4ce3177b304455e7ec857fd79fb9f
Srishti-Khandelwal16 <srishtikhandelwal1604@gmail.com>
2021-07-01 11:47:07 +0530

    updated environment for swagger ui

--- SampleApiProject/Startup.cs
+++ SampleApiProject/Startup.cs
@@ -1,17 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SampleApiProject
 {
@@ -38,12 +30,12 @@ namespace SampleApiProject
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleApiProject v1"));
-            }
+            //}
 
             app.UseHttpsRedirection();
 
